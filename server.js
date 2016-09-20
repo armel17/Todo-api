84f335c262b52f3c58fde26a7eb53fa4d9988e65
Arnaud Melin <arnaudmelin@hotmail.com>
2016-09-20 22:10:58 +0100

    Update DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -132,20 +132,55 @@ app.post('/todos', function (req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
+
+    // NEW - Real database
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function (rowsDeleted) {
+        if (rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with id'
+            });
+        } else {
+            res.status(204).send();
+        }
+    }, function () {
+        res.status(500).send();
     });
 
-    if (!matchedTodo) {
-        res.status(404).json({
-            "error": "no todo found with that id."
-        });
-    } else {
-        // Delete the item
-        todos = _.without(todos, matchedTodo);
-        // Return the deleted item (+ a 200 Status)
-        res.json(matchedTodo);
-    }
+    // OTHER METHOD:
+    //    db.todo.findById(todoId)
+    //        .then(function (matchedTodo) {
+    //            if (matchedTodo) {
+    //                res.json(matchedTodo);
+    //                return matchedTodo.destroy({
+    //                    force: true
+    //                });
+    //            } else {
+    //                res.status(404).send();
+    //            }
+    //        })
+    //        .catch(function (e) {
+    //            res.status(400).send(e);
+    //        });
+
+    // OLD - static array
+    //    var matchedTodo = _.findWhere(todos, {
+    //        id: todoId
+    //    });
+    //
+    //    if (!matchedTodo) {
+    //        res.status(404).json({
+    //            "error": "no todo found with that id."
+    //        });
+    //    } else {
+    //        // Delete the item
+    //        todos = _.without(todos, matchedTodo);
+    //        // Return the deleted item (+ a 200 Status)
+    //        res.json(matchedTodo);
+    //    }
 });
 
 // PUT /todos/:id
